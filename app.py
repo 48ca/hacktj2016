e5956ec5c7edb4d9f7a3a947181dd0b52ca5deab
jamesthoughton <jamesthoughton@gmail.com>
2016-02-06 19:33:48 -0500

    Add some User stuff

--- app.py
+++ app.py
@@ -4,25 +4,34 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, session, request, send_from_directory, request, abort, flash, redirect, url_for
 import os
 
-DEBUG = True
-SECRET_KEY = 'memes'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db/sql.db'
-# SQLALCHEMY_TRACK_MODIFICATIONS = True
+from database import init_db
+
+SQLALCHEMY_DATABASE_URI = "postgresql://cov:memes@localhost/covalence"
 
 app = Flask(__name__)
 app.secret_key = 'Bc9on)Dhb308rj98HER87g67rN*&^F767h^&*#HH(F3b987vbf9H#'
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 login_manager = LoginManager(app)
 login_manager.init_app(app)
- 
+
+init_db() 
+
 db = SQLAlchemy(app)
- 
+
 # Models
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+	def __repr__(self):
+		return username
 
 # Routes
 
@@ -35,10 +44,10 @@ def init_request():
 def index():
 	return render_template('index.html')
 
-@app.route('/login',methods=['GET','POST'])
-def login():
+@app.route('/register',methods=['GET','POST'])
+def register():
 	if request.method == 'GET':
-		return render_template('login.html', next=request.args.get('next'))
+		return render_template('register.html', next=request.args.get('next'))
 	elif request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
@@ -48,28 +57,45 @@ def login():
 			user = User(username=username, password=password)
 			db.session.add(user)
 			db.session.commit()
-		 
-			flash('You have registered the username {0}. Please login'.format(username))
+
+			# flash('You have registered the username {0}. Please login'.format(username))
 			return redirect(url_for('login'))
+			# return redirect(url_for('home'))
 		else:
 			flash('The username {0} is already in use.  Please try a new username.'.format(username))
 			return redirect(url_for('register'))
 	else:
 		abort(405)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    elif request.method == 'POST':
-        pass
-    else:
-        abort(405)
+@app.route('/login',methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html', next=request.args.get('next'))
+	elif request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		 
+		user = User.query.filter_by(username=username).filter_by(password=password)
+		if user.count() == 1:
+			login(user.one())
+			try:
+				next = request.form['next']
+				return redirect(url_for(next))
+			except:
+				return redirect(url_for('home'))
+			# flash('You have registered the username {0}. Please login'.format(username))
+			return redirect(url_for('home'))
+		else:
+			flash('Incorrect username and password combination: {} {}'.format(username,password))
+			return redirect(url_for('login'))
+	else:
+		abort(405)
 
-@app.route('/secret')
+
+@app.route('/home')
 @login_required
-def secret():
-	return render_template('secret.html')
+def home():
+	return render_template('home.html')
 
 @app.route('/static/<path:path>')
 def send_static():
--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+# engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+engine = create_engine('postgresql://cov:memes@localhost/covalence')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -13,5 +14,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import covalent.models
+    # import covalent.models
     Base.metadata.create_all(bind=engine)
